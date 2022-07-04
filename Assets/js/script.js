494001a2908dab23b416f90fd9eb6f03ec2cc41d
TDEvans97 <tylerdevansmusic@gmail.com>
2022-07-04 16:38:00 -0400

    key:values are saved to local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,14 @@
 let currentDay = moment();
-let textareaEl = $("textarea");
-// Select all buttons as a NodeList
+// Select all buttons as a NodeList.
 let buttonEl = document.querySelectorAll(".saveBtn");
-// Convert the NodeList to an array
+// Convert the NodeList to an array.
 let buttonElArr = Array.from(buttonEl);
 
 // Display the current day at the top of the calendar.
 $("#currentDay").text(currentDay.format("dddd, MMMM Do"));
 
+// Add an event listener to each button.
 for (i = 0; i < buttonElArr.length; i++) {
-    console.log(buttonElArr[i])
     buttonElArr[i].addEventListener("click", saveUserTasks);
 }
 
@@ -17,8 +16,7 @@ function saveUserTasks() {
     console.log("Hello")
     // Grab the hour and textarea value from siblings of the clicked button.
     let hour = $(this).siblings(".hour").text();
-    let task = $(this).siblings(".description").val();
-    // console.log(task);
+    let task = $(this).siblings(".description").children(".text-input").val();
     if (task === "") {
         alert("You have not yet entered a task to save.");
     } else {
