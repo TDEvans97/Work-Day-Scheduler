fa5443ee9f4de1bdc7a924edfbe3e70330a7d7f9
TDEvans97 <tylerdevansmusic@gmail.com>
2022-07-04 16:15:30 -0400

    Fixed the button click functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,37 +1,31 @@
 let currentDay = moment();
 let textareaEl = $("textarea");
+// Select all buttons as a NodeList
+let buttonEl = document.querySelectorAll(".saveBtn");
+// Convert the NodeList to an array
+let buttonElArr = Array.from(buttonEl);
 
-// Display the current day at the top of the calendar
+// Display the current day at the top of the calendar.
 $("#currentDay").text(currentDay.format("dddd, MMMM Do"));
 
-//Add an event listener to each button
-$(".saveBtn").click(saveUserTasks);
+for (i = 0; i < buttonElArr.length; i++) {
+    console.log(buttonElArr[i])
+    buttonElArr[i].addEventListener("click", saveUserTasks);
+}
 
 function saveUserTasks() {
+    console.log("Hello")
     // Grab the hour and textarea value from siblings of the clicked button.
     let hour = $(this).siblings(".hour").text();
     let task = $(this).siblings(".description").val();
-    console.log(task);
+    // console.log(task);
     if (task === "") {
         alert("You have not yet entered a task to save.");
     } else {
         localStorage.setItem(hour, task);
     }
-    console.log("clicked");
 };
 
-// buttonEl.on("click", saveUserTasks());
-
-// for (let button of buttonElArr.values()) {
-    //     button.addEventListener("click", saveUserTasks());
-    //     console.log(button);
-    // }
-    // for (i = 0; i < buttonEl.length; i++) {
-    //     buttonEl[i].addEventListener("click", saveUserTasks());
-    // }
-    
-    
-    
-// if the hour has past, add attr class .past for gray, 
-// .present for red, 
+// if the hour has past, add attr class .past for gray,
+// .present for red,
 // else .future for green
